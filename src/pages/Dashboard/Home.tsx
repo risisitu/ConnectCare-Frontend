b2663cf69a3429243ac6777cc2eec1ff4a14b648
Risittan <risittan@gmail.com>
2026-01-11 17:02:30 +0000

    added doctors upcoming appointments table on dashboard

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -2,9 +2,9 @@ import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
 import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
 import StatisticsChart from "../../components/ecommerce/StatisticsChart";
 import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
-import RecentOrders from "../../components/ecommerce/RecentOrders";
 import DemographicCard from "../../components/ecommerce/DemographicCard";
 import PageMeta from "../../components/common/PageMeta";
+import DashboardAppointmentsTable from "../../components/Appointments/DashboardAppointmentsTable";
 
 export default function Home() {
   return (
@@ -33,7 +33,7 @@ export default function Home() {
         </div>
 
         <div className="col-span-12 xl:col-span-7">
-          <RecentOrders />
+          <DashboardAppointmentsTable />
         </div>
       </div>
     </>
